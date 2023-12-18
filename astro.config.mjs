81fe3b59067d3af69408e3cbe623f6faa698f87c
Nambi Sankaran <nambisankaran@lucidmotors.com>
2023-12-17 16:44:18 -0800

    added rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://peacock-1234.netlify.app/",
+  integrations: [preact()]
+});
